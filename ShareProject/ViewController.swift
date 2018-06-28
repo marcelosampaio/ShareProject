b0dbae3645c0e748c88e2a9d980adc15a3c6a0e9
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-06-28 18:07:59 -0300

    initial commit 🎾

--- ShareProject/ViewController.swift
+++ ShareProject/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Marcelo on 6/28/18.
 //  Copyright © 2018 Marcelo. All rights reserved.
 //
+//  Base Article: http://www.yudiz.com/share-extention-in-ios-app/
 
 import UIKit
 
@@ -12,14 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
