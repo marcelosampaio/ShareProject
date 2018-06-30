5ee82423b6f1ae18d58ab6907461b1051d04c00e
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-06-30 00:41:23 -0300

    checkpoint commit 🌍

--- ShareExtension/CustomShareController.swift
+++ ShareExtension/CustomShareController.swift
@@ -8,15 +8,57 @@
 
 import UIKit
 import Social
-
+import MobileCoreServices
 
 class CustomShareController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let content = extensionContext!.inputItems[0] as! NSExtensionItem
+        let contentType = kUTTypeImage as String
+        
+        for attachment in content.attachments as! [NSItemProvider] {
+            if attachment.hasItemConformingToTypeIdentifier(contentType) {
+                
+                attachment.loadItem(forTypeIdentifier: contentType, options: nil) { data, error in
+                    if error == nil {
+                        let url = data as! URL
+                        if let imageData = NSData.init(contentsOf: url) {
+                            // load image
+                            self.imageView.image = UIImage(data: imageData as Data)
+                        }
+                    } else {
+                        
+//                        let alert = UIAlertController(title: "Error", message: "Error loading image", preferredStyle: .Alert)
+//
+//                        let action = UIAlertAction(title: "Error", style: .Cancel) { _ in
+//                            self.dismissViewControllerAnimated(true, completion: nil)
+//                        }
+//
+//                        alert.addAction(action)
+//                        self.presentViewController(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        print("🎾 content: \(content)")
+        print(".....")
     }
     
     // MARK: - UI Actions
